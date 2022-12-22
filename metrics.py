7d86807a73b6177147cd1ead9174c0763d215cef
Luke Zhang <t116517uhn@h4huhnlogin2.uhnh4h.cluster>
2022-12-21 22:27:28 -0500

    spare scripts for testing and visualization

---
+++ metrics.py
@@ -0,0 +1,54 @@
+import sklearn.metrics
+import pandas as pd
+import matplotlib.pyplot as plt
+
+model_name = '2022-12-15_full_e200_lr-4_dropout-0.3'
+df = pd.read_csv(f'llps-v2/output/{model_name}_eval_log.csv')
+df_pos = pd.read_csv('llps-v2/data/ext_pos.csv')
+df_pos.columns = ['sequences']
+df_neg = pd.read_csv('llps-v2/data/ext_neg.csv')
+df_neg.columns = ['sequences']
+df_seqs = pd.concat([df_pos, df_neg]).reset_index(drop=True)
+df_seqs = df_seqs.iloc[0:319]
+df = pd.concat([df, df_seqs], axis=1)
+print(df)
+df2 = df[df.iloc[:,2].str.len() <= 1800].reset_index(drop=True)
+print(df2)
+
+y_score = df2['scores']
+y_true = df2['labels']
+
+correct = 0
+total = 0
+for i in range(len(y_score)):
+    total += 1
+    if abs(y_score[i] - y_true[i]) < 0.5:
+        correct += 1 
+
+print('=====================METRICS======================')
+
+print(f'Accuracy: {correct/total}. {correct}/{total}')
+
+auroc = sklearn.metrics.roc_auc_score(y_true, y_score)
+print(f'AUROC: {auroc}')
+fpr, tpr, thresholds = sklearn.metrics.roc_curve(y_true, y_score)
+plt.figure(0)
+plt.plot(fpr, tpr)
+plt.title(f'{model_name} ROC')
+plt.xlabel('FPR')
+plt.ylabel('TPR')
+plot_f = f'llps-v2/figures/{model_name}_roc2.png'
+plt.savefig(fname=plot_f)
+print(f'saved to {plot_f}')
+
+auprc = sklearn.metrics.average_precision_score(y_true, y_score)
+print(f'AUPRC: {auprc}')
+precision, recall, thresholds = sklearn.metrics.roc_curve(y_true, y_score)
+plt.figure(1)
+plt.plot(recall, precision)
+plt.title(f'{model_name} PRC')
+plt.xlabel('Recall')
+plt.ylabel('Precision')
+plot_f = f'llps-v2/figures/{model_name}_prc2.png'
+plt.savefig(fname=plot_f)
+print(f'saved to {plot_f}')
---
+++ test.py
@@ -0,0 +1,47 @@
+import torch
+import sklearn.metrics
+import matplotlib.pyplot as plt
+from model import Model
+from dataset import SingleFileTestDataset, SingleFileDataset
+from torch.utils.data import DataLoader
+import time
+
+start_time = time.time()
+
+print('=====================HYPERPARAMS======================')
+num_epochs = 200
+learning_rate = 1e-4
+num_layers = 1
+model_dim = 320
+num_heads = 4
+ff_dim = 320
+random_seed = 69
+batch_size = 5
+dropout = 0.3
+loss_function = torch.nn.BCELoss()
+model_name = '2022-12-15_full_e200_lr-4_dropout-0.3'
+path = '/cluster/projects/kumargroup/luke/output/v2/'
+
+print(f'model_name: {model_name}')
+torch.manual_seed(random_seed)
+
+print('=====================MODEL======================')
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+my_model = Model(device, 1, 320, 4, 320, 0.3)
+my_model.load_state_dict(torch.load(f'{path}{model_name}.pt'))
+print(my_model)
+my_model.eval()
+
+print('=====================DATA======================')
+
+data = SingleFileDataset('llps-v2/data/training_data_features.csv')
+
+dataloader = DataLoader(data, batch_size=batch_size, shuffle=True)
+
+test = next(iter(dataloader))
+print(test)
+
+end_time = time.time()
+
+print(f'Elapsed: {end_time - start_time}')
