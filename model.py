1d1d2df6a573d734b87cfb4723c58678a23b5d12
Luke Zhang <t116517uhn@h4huhnlogin2.uhnh4h.cluster>
2023-01-04 18:37:59 -0500

    verbose method for size printing in forward call

--- model.py
+++ model.py
@@ -4,28 +4,40 @@ from encoder import Encoder
 from esm_pretrained import ESM
 
 class Model (torch.nn.Module):
-    def __init__(self, device, num_layers, model_dim, num_heads, ff_dim, dropout=0.5) -> None:
+    def __init__(self, device, num_layers, model_dim, num_heads, ff_dim, dropout=0.5, verbose=False) -> None:
         super().__init__()
         self.esm = ESM(embed_dim=model_dim)
         self.device = device
         self.encoder = Encoder(num_layers=num_layers, model_dim=model_dim, num_heads=num_heads, ff_dim=ff_dim, dropout=dropout)
         self.classifier = AdaptiveClassifier(model_dim=model_dim)
+        self.verbose = verbose
 
         self.encoder.to(self.device)
         self.classifier.to(self.device)
 
     def forward(self, x):
+        if self.verbose:
+            print(x)
         x = self.esm.get_representation(x)
+        if self.verbose:
+            print(x.size())
         x = x.to(self.device)
+        if self.verbose:
+            print(x.size())
         x = self.encoder(x)
-        return self.classifier(x)
+        if self.verbose:
+            print(x.size())
+        x = self.classifier(x)
+        if self.verbose:
+            print(x.size())
+        return x
 
 
 if __name__ == '__main__':
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(device)
-    my_model = Model(device, num_layers=1, model_dim=320, num_heads=4, ff_dim=320, dropout=0.3)
+    my_model = Model(device, num_layers=1, model_dim=320, num_heads=4, ff_dim=320, dropout=0.3, verbose=True)
     print(my_model)
     test = [(0, 'AGFQPQSQGMSLNDFQKQQKQAAPKPKKTLKLVSSSGIKLANATKKVGTKPAESDKKEEEKSAETKEPTKEPTKVEEPVKKEEKPVQTEEKTEEKSELPKVEDLKISESTHNTNNANVTSADALIKEQEEEVDDEVVNDMFGGKDHVSLIFMGHVDAGKSTMGGNLLYLTGSVDKRTIEKYEREAKDAGRQGWYLSWVMDTNKEERNDGKTIEVGKAYFETEKRRYTILDAPGHKMYVSEMIGGASQADVGVLVISARKGEYETGFERGGQTREHALLAKTQGVNKMVVVVNKMDDPTVNWSKERYDQCVSNVSNFLRAIGYNIKTDVVFMPVSGYSGANLKDHVDPKECPWYTGPTLLEYLDTMNHVDRHINAPFMLPIAAKMKDLGTIVEGKIESGHIKKGQSTLLMPNKTAVEIQNIYNETENEVDMAMCGEQVKLRIKGVEEEDISPGFVLTSPKNPIKSVTKFVAQIAIVELKSIIAAGFSCVMHVHTAIEEVHIVKLLHKLEKGTNRKSKKPPAFAKKGMKVIAVLETEAPVCVETYQDYPQLGRFTLRDQGTTIAIGKIVKIAE')]
     a = my_model(test)
