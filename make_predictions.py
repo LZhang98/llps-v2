5b94b78a8372902cfbf234b854e9608768883c43
Luke Zhang <lzhang98@cedar5.cedar.computecanada.ca>
2023-05-12 10:50:19 -0700

    create gitignore, update predictions to config values

--- make_predictions.py
+++ make_predictions.py
@@ -3,8 +3,10 @@ from model import Model
 import time
 import sys
 import pandas as pd
+import config
 
 start_time = time.time()
+
 
 # Script input args
 model_name = sys.argv[1]
@@ -13,7 +15,7 @@ output_name = sys.argv[3]
 if len(sys.argv) == 5:
     output_dir = sys.argv[4]
 else:
-    output_dir = 'llps-v2/predictions/'
+    output_dir = config.model['output_dir']
 
 # Set up data input and output
 output_file = f'{output_dir}/{model_name}_{output_name}.csv'
@@ -26,7 +28,7 @@ data['length'] = data['sequence'].str.len()
 # Initialize model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 my_model = Model(device, 1, 320, 4, 320, 0.3, False, is_eval=True)
-path = '/cluster/projects/kumargroup/luke/output/v2/'
+path = config.model['model_dir']
 my_model.load_state_dict(torch.load(f'{path}{model_name}.pt', map_location=torch.device(device)))
 
 num_samples = len(data)
